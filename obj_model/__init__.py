5dca85e2f04a6783a2a8f81844e1564c6a641fd1
Jonathan Karr <jonrkarr@gmail.com>
2018-03-12 17:25:10 -0400

    adding API

--- obj_model/__init__.py
+++ obj_model/__init__.py
@@ -3,3 +3,19 @@ import pkg_resources
 with open(pkg_resources.resource_filename('obj_model', 'VERSION'), 'r') as file:
     __version__ = file.read().strip()
 # :obj:`str`: version
+
+# API
+from .core import (Model, Attribute,
+                   LiteralAttribute, NumericAttribute, EnumAttribute, BooleanAttribute, FloatAttribute,
+                   IntegerAttribute, PositiveIntegerAttribute, StringAttribute, LongStringAttribute,
+                   RegexAttribute, SlugAttribute, UrlAttribute, DateAttribute, TimeAttribute,
+                   DateTimeAttribute, RelatedAttribute, OneToOneAttribute, OneToManyAttribute,
+                   ManyToOneAttribute, ManyToManyAttribute,
+                   InvalidObjectSet, InvalidModel, InvalidObject, InvalidAttribute, Validator,
+                   ObjModelWarning, SchemaWarning,
+                   ModelSource, TabularOrientation,
+                   get_models, get_model, excel_col_name)
+from . import abstract
+from . import extra_attributes
+from . import io
+from . import utils
---
+++ tests/test_api.py
@@ -0,0 +1,16 @@
+""" 
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2018-03-12
+:Copyright: 2018, Karr Lab
+:License: MIT
+"""
+
+import obj_model
+import types
+import unittest
+
+
+class ApiTestCase(unittest.TestCase):
+    def test(self):
+        self.assertIsInstance(obj_model.Model, type)
+        self.assertIsInstance(obj_model.abstract, types.ModuleType)
