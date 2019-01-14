72e62433b4f50194a66f73d17eff443ca96ea7a6
Jonathan Karr <jonrkarr@gmail.com>
2019-01-14 11:52:56 -0500

    adding ontology attribute

---
+++ obj_model/ontology.py
@@ -0,0 +1,148 @@
+""" Ontology attributes
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2019-01-14
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+from . import core
+import pronto
+
+
+class OntologyAttribute(core.LiteralAttribute):
+    """ Ontology attribute
+
+    Attributes:
+        ontology (obj:`pronto.Ontology`): ontology
+        none (:obj:`bool`): if :obj:`False`, the attribute is invalid if its value is :obj:`None`
+    """
+
+    def __init__(self, ontology, none=True, default=None, default_cleaned_value=None, verbose_name='', help='',
+                 primary=False, unique=False, unique_case_insensitive=False):
+        """
+        Args:
+            ontology (:obj:`pronto.Ontology`): ontology
+            none (:obj:`bool`, optional): if :obj:`False`, the attribute is invalid if its value is :obj:`None`
+            default (:obj:`pronto.term.Term`, optional): default value
+            default_cleaned_value (:obj:`pronto.term.Term`, optional): value to replace
+                :obj:`None` values with during cleaning
+            verbose_name (:obj:`str`, optional): verbose name
+            help (:obj:`str`, optional): help string
+            primary (:obj:`bool`, optional): indicate if attribute is primary attribute
+            unique (:obj:`bool`, optional): indicate if attribute value must be unique
+            unique_case_insensitive (:obj:`bool`, optional): if true, conduct case-insensitive test of uniqueness
+
+        Raises:
+            :obj:`ValueError`: if :obj:`ontology` is not an instance of :obj:`pronto.Ontology`,
+            :obj:`ValueError`: if :obj:`default` not in :obj:`ontology`
+            :obj:`ValueError`: if :obj:`default_cleaned_value` not in :obj:`ontology`
+        """
+        if not isinstance(ontology, pronto.Ontology):
+            raise ValueError('`ontology` must be an instance of `pronto.Ontology`')
+        if default is not None and \
+                (not isinstance(default, pronto.term.Term) or default not in ontology):
+            raise ValueError(
+                '`default` must be `None` or in `ontology`')
+        if default_cleaned_value is not None and \
+                (not isinstance(default_cleaned_value, pronto.term.Term) or default_cleaned_value not in ontology):
+            raise ValueError(
+                '`default_cleaned_value` must be `None` or in `ontology`')
+
+        super(OntologyAttribute, self).__init__(default=default,
+                                                default_cleaned_value=default_cleaned_value,
+                                                verbose_name=verbose_name, help=help,
+                                                primary=primary, unique=unique, unique_case_insensitive=unique_case_insensitive)
+
+        self.ontology = ontology
+        self.none = none
+
+    def clean(self, value):
+        """ Convert attribute value into the appropriate type
+
+        Args:
+            value (:obj:`object`): value of attribute to clean
+
+        Returns:
+            :obj:`pronto.term.Term` or :obj:`None`: cleaned value
+            :obj:`core.InvalidAttribute` or :obj:`None`: cleaning error
+        """
+        error = None
+
+        if value and isinstance(value, str):
+            value = self.ontology.get(value, None)
+            if value is None:
+                error = 'Value "{}" is not in `ontology`'.format(value)
+
+        elif value is None or value == '':
+            value = self.get_default_cleaned_value()
+
+        elif not (isinstance(value, pronto.term.Term) and value in self.ontology):
+            error = "Value '{}' must be in `ontology`".format(value)
+
+        if error:
+            return (None, core.InvalidAttribute(self, [error]))
+        else:
+            return (value, None)
+
+    def validate(self, obj, value):
+        """ Determine if `value` is a valid value of the attribute
+
+        Args:
+            obj (:obj:`Model`): object being validated
+            value (:obj:`pronto.term.Term`): value of attribute to validate
+
+        Returns:
+            :obj:`core.InvalidAttribute` or :obj:`None`: :obj:`None` if attribute is valid, other return list of
+                errors as an instance of :obj:`core.InvalidAttribute`
+        """
+        if value is None:
+            if not self.none:
+                return core.InvalidAttribute(self, ['Value cannot be `None`'])
+
+        elif not isinstance(value, pronto.term.Term) or value not in self.ontology:
+            return core.InvalidAttribute(self, ["Value '{}' must be in `ontology`".format(value)])
+
+        return None
+
+    def serialize(self, value):
+        """ Serialize ontology instance
+
+        Args:
+            value (:obj:`pronto.term.Term`): Python representation
+
+        Returns:
+            :obj:`str`: simple Python representation
+        """
+        if value:
+            return value.id
+        return ''
+
+    def to_builtin(self, value):
+        """ Encode a value of the attribute using a simple Python representation (dict, list, str, float, bool, None)
+        that is compatible with JSON and YAML
+
+        Args:
+            value (:obj:`pronto.term.Term`): value of the attribute
+
+        Returns:
+            :obj:`str`: simple Python representation of a value of the attribute
+        """
+        if value:
+            return value.id
+        return None
+
+    def from_builtin(self, json):
+        """ Decode a simple Python representation (dict, list, str, float, bool, None) of a value of the attribute
+        that is compatible with JSON and YAML
+
+        Args:
+            json (:obj:`str`): simple Python representation of a value of the attribute
+
+        Returns:
+            :obj:`pronto.term.Term`: decoded value of the attribute
+        """
+        if json:
+            return self.ontology[json]
+        else:
+            return None
---
+++ tests/test_ontology.py
@@ -0,0 +1,88 @@
+""" Test ontology attributes
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2019-01-14
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+from obj_model import core
+import obj_model.ontology
+import pronto
+import unittest
+
+
+class OntologyAttributeTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.Ontology = pronto.Ontology('tests/fixtures/SBO.obo')
+        cls.term = cls.Ontology['SBO:0000000']
+
+    def test_init(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        with self.assertRaisesRegex(ValueError, 'be an instance of `pronto.Ontology`'):
+            attr = obj_model.ontology.OntologyAttribute('NOT_AN_ONTOLOGY')
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology, default=self.term)
+        with self.assertRaisesRegex(ValueError, 'must be `None` or in `ontology`'):
+            attr = obj_model.ontology.OntologyAttribute(self.Ontology, default='NOT_A_TERM')
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology, default_cleaned_value=self.term)
+        with self.assertRaisesRegex(ValueError, 'must be `None` or in `ontology`'):
+            attr = obj_model.ontology.OntologyAttribute(self.Ontology, default_cleaned_value='NOT_A_TERM')
+
+    def test_clean(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.clean('SBO:0000000'), (self.term, None))
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.clean(None), (None, None))
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.clean(''), (None, None))
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.clean(self.term)[1], None)
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology, default_cleaned_value=self.term)
+        self.assertEqual(attr.clean(None)[0].id, self.term.id)
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertNotEqual(attr.clean('NOT_A_TERM')[1], None)
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertNotEqual(attr.clean(pronto.term.Term('NOT_A_TERM'))[1], None)
+
+    def test_validate(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.validate(None, self.term), None)
+        self.assertEqual(attr.validate(None, None), None)
+        self.assertNotEqual(attr.validate(None, 'SBO:0000000'), None)
+
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology, none=False)
+        self.assertNotEqual(attr.validate(None, None), None)
+
+    def test_serialize(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.serialize(self.term), 'SBO:0000000')
+        self.assertEqual(attr.serialize(None), '')
+        self.assertEqual(attr.serialize(''), '')
+
+    def test_deserialize(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.deserialize('SBO:0000000')[0].id, 'SBO:0000000')
+        self.assertEqual(attr.deserialize(None), (None, None))
+        self.assertEqual(attr.deserialize(''), (None, None))
+
+    def test_to_builtin(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.to_builtin(self.term), 'SBO:0000000')
+        self.assertEqual(attr.to_builtin(None), None)
+        self.assertEqual(attr.to_builtin(''), None)
+
+    def test_from_builtin(self):
+        attr = obj_model.ontology.OntologyAttribute(self.Ontology)
+        self.assertEqual(attr.from_builtin('SBO:0000000').id, 'SBO:0000000')
+        self.assertEqual(attr.from_builtin(None), None)
+        self.assertEqual(attr.from_builtin(''), None)
