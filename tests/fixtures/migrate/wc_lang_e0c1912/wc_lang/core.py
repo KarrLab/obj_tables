cd6e184902008acbab79df0e8144f6b75900c31a
Jonathan Karr <jonrkarr@gmail.com>
2019-03-26 22:51:08 -0400

    updating for renaming in ontology

--- tests/fixtures/migrate/wc_lang_e0c1912/wc_lang/core.py
+++ tests/fixtures/migrate/wc_lang_e0c1912/wc_lang/core.py
@@ -3807,7 +3807,7 @@ class Change(obj_model.Model):
     model = ManyToOneAttribute(Model, related_name='changes')
 
     type = OntologyAttribute(onto, namespace='WC',
-                             terms=onto['WC:change_provenance'].rchildren())
+                             terms=onto['WC:change'].rchildren())
     target = LongStringAttribute()
     target_submodel = ManyToOneAttribute(Submodel, related_name='changes')
     target_type = OntologyAttribute(onto, namespace='WC',
