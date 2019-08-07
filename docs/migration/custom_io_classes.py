708dc4a1d9ac0478eabb78b960a213a24f55b3d0
Arthur Goldberg <artgoldberg@gmail.com>
2019-08-06 20:35:56 -0400

    finish drafting the Example configuration files section

--- docs/migration/custom_io_classes.py
+++ docs/migration/custom_io_classes.py
@@ -1,6 +1,5 @@
 """ Import wc_lang's Reader so it can be used by obj_model migration
-    of wc_lang model files
-"""
+    of wc_lang model files """
 
 # this file is imported by obj_model/migrate.py
 from wc_lang.io import Reader
