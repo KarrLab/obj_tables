7861acf6d82ec26295e8c88172a680c76c318809
Jonathan Karr <jonrkarr@gmail.com>
2020-03-06 10:12:06 -0500

    adding attribute for quantities

--- obj_tables/sci/units.py
+++ obj_tables/sci/units.py
@@ -10,11 +10,13 @@ from .. import core
 from wc_utils.util.list import det_dedupe
 from wc_utils.util.units import are_units_equivalent
 import itertools
+import math
 import pint
 import wc_utils.workbook.io
 
 __all__ = [
     'UnitAttribute',
+    'QuantityAttribute',
 ]
 
 
@@ -35,8 +37,8 @@ class UnitAttribute(core.LiteralAttribute):
             registry (:obj:`pint.UnitRegistry`): unit registry
             choices (:obj:`tuple` of :obj:`pint.unit._Unit`, optional): allowed units
             none (:obj:`bool`, optional): if :obj:`False`, the attribute is invalid if its value is :obj:`None`
-            default (:obj:`str`, optional): default value
-            default_cleaned_value (:obj:`str`, optional): value to replace :obj:`None` values with during cleaning
+            default (:obj:`registry.Unit`, optional): default value
+            default_cleaned_value (:obj:`registry.Unit`, optional): value to replace :obj:`None` values with during cleaning
             none_value (:obj:`object`, optional): none value
             verbose_name (:obj:`str`, optional): verbose name
             description (:obj:`str`, optional): description
@@ -272,6 +274,258 @@ class UnitAttribute(core.LiteralAttribute):
         return validation
 
 
+class QuantityAttribute(core.LiteralAttribute):
+    """ Quantity (magntitude and units) attribute
+
+    Attributes:
+        registry (:obj:`pint.UnitRegistry`): unit registry
+        choices (:obj:`tuple` of :obj:`pint.unit._Unit`): allowed units
+        none (:obj:`bool`): if :obj:`False`, the attribute is invalid if its value is :obj:`None`
+    """
+
+    def __init__(self, registry, choices=None, none=True, default=None, default_cleaned_value=None,
+                 none_value=None, verbose_name='', description="Units (e.g. 'second', 'meter', or 'gram')",
+                 primary=False, unique=False, unique_case_insensitive=False):
+        """
+        Args:
+            registry (:obj:`pint.UnitRegistry`): unit registry
+            choices (:obj:`tuple` of :obj:`pint.unit._Unit`, optional): allowed units
+            none (:obj:`bool`, optional): if :obj:`False`, the attribute is invalid if its value is :obj:`None`
+            default (:obj:`pint.unit.unit`, optional): default value
+            default_cleaned_value (:obj:`str`, optional): value to replace :obj:`None` values with during cleaning
+            none_value (:obj:`object`, optional): none value
+            verbose_name (:obj:`str`, optional): verbose name
+            description (:obj:`str`, optional): description
+            primary (:obj:`bool`, optional): indicate if attribute is primary attribute
+            unique (:obj:`bool`, optional): indicate if attribute value must be unique
+            unique_case_insensitive (:obj:`bool`, optional): if true, conduct case-insensitive test of uniqueness
+
+        Raises:
+            :obj:`ValueError`: if registry is not an instance of `pint.UnitRegistry`
+            :obj:`ValueError`: if default is not an instance of `registry.Unit`
+            :obj:`ValueError`: if default_cleaned_value is not an instance of `registry.Unit`
+            :obj:`ValueError`: if a choice is not an instance of `registry.Unit`
+        """
+        if not isinstance(registry, pint.UnitRegistry):
+            raise ValueError('`registry` must be an instance of `pint.UnitRegistry`')
+        if default is not None and not isinstance(default, registry.Quantity):
+            raise ValueError('`default` must be an instance of `registry.Quantity`')
+        if default_cleaned_value is not None and not isinstance(default_cleaned_value, registry.Quantity):
+            raise ValueError('`default_cleaned_value` must be an instance of `registry.Quantity`')
+
+        if choices is not None:
+            for choice in choices:
+                if not isinstance(choice, registry.Unit):
+                    raise ValueError('choices must be instances of `registry.Unit`')
+
+        super(QuantityAttribute, self).__init__(default=default,
+                                                default_cleaned_value=default_cleaned_value, none_value=none_value,
+                                                verbose_name=verbose_name, description=description,
+                                                primary=primary, unique=unique, unique_case_insensitive=unique_case_insensitive)
+
+        self.registry = registry
+        self.choices = choices
+        self.none = none
+
+    def get_default(self):
+        """ Get default value for attribute
+
+        Returns:
+            :obj:`pint.quantity._Quantity`: initial value
+        """
+        return self.default
+
+    def get_default_cleaned_value(self):
+        """ Get value to replace :obj:`None` values with during cleaning
+
+        Returns:
+            :obj:`pint.quantity._Quantity`: initial value
+        """
+        return self.default_cleaned_value
+
+    def value_equal(self, val1, val2, tol=1e-12):
+        """ Determine if attribute values are equal
+
+        Args:
+            val1 (:obj:`pint.quantity._Quantity`): first value
+            val2 (:obj:`pint.quantity._Quantity`): second value
+            tol (:obj:`float`, optional): equality tolerance
+
+        Returns:
+            :obj:`bool`: True if attribute values are equal
+        """
+        if not isinstance(val1, pint.quantity._Quantity) or not isinstance(val2, pint.quantity._Quantity):
+            return val1 == val2
+
+        if not are_units_equivalent(val1.units, val2.units, check_same_magnitude=False):
+            return False
+        mag1 = val1.m
+        mag2 = val2.to(val1.units).m
+        return mag1 == mag2 or \
+            (math.isnan(mag1) and math.isnan(mag2)) or \
+            (mag1 == 0. and abs(mag2) < tol) or \
+            (mag1 != 0. and abs((mag1 - mag2) / mag1) < tol)
+
+    def clean(self, value):
+        """ Convert attribute value into the appropriate type
+
+        Args:
+            value (:obj:`object`): value of attribute to clean
+
+        Returns:
+            :obj:`tuple` of :obj:`str`, :obj:`core.InvalidAttribute` or :obj:`None`: tuple of cleaned value and cleaning error
+        """
+        error = None
+        if isinstance(value, self.registry.Quantity):
+            pass
+        elif value is None or value == '':
+            value = self.get_default_cleaned_value()
+        elif isinstance(value, str):
+            try:
+                value = self.registry.parse_expression(value)
+            except (pint.UndefinedUnitError, TypeError):
+                error = core.InvalidAttribute(self, ['Invalid quantity {}'.format(value)])
+        else:
+            error = core.InvalidAttribute(self, ['Invalid quantity {}'.format(value)])
+        return (value, error)
+
+    def copy_value(self, value, objects_and_copies):
+        """ Copy value
+
+        Args:
+            value (:obj:`pint.quantity._Quantity`): value
+            objects_and_copies (:obj:`dict`): dictionary that maps objects to their copies
+
+        Returns:
+            :obj:`pint.quantity._Quantity`: copy of value
+        """
+        return self.registry.Quantity(value.m, value.units)
+
+    def validate(self, obj, value):
+        """ Determine if `value` is a valid value for this QuantityAttribute
+
+        Args:
+            obj (:obj:`Model`): class being validated
+            value (:obj:`pint.quantity._Quantity`): value of attribute to validate
+
+        Returns:
+            :obj:`core.InvalidAttribute` or None: None if attribute is valid, other return list of
+                errors as an instance of :obj:`core.InvalidAttribute`
+        """
+        error = None
+
+        if value is None:
+            if not self.none:
+                error = 'Value must be an instance of `registry.Quantity`'
+
+        elif not isinstance(value, self.registry.Quantity):
+            error = 'Value must be an instance of `registry.Quantity`'
+
+        else:
+            value = self.registry.parse_expression(str(value))
+            if self.choices:
+                valid = False
+                for choice in self.choices:
+                    try:
+                        value.to(choice)
+                        valid = True
+                        break
+                    except pint.DimensionalityError:
+                        pass
+                if not valid:
+                    error = f"Value must be compatible with `choices`: {set([str(c) for c in self.choices])}"
+
+        if error:
+            return core.InvalidAttribute(self, [error])
+        return None
+
+    def serialize(self, value):
+        """ Serialize string
+
+        Args:
+            value (:obj:`pint.quantity._Quantity`): Python representation
+
+        Returns:
+            :obj:`str`: simple Python representation
+        """
+        if value:
+            return str(value)
+        else:
+            return ''
+
+    def to_builtin(self, value):
+        """ Encode a value of the attribute using a simple Python representation (dict, list, str, float, bool, None)
+        that is compatible with JSON and YAML
+
+        Args:
+            value (:obj:`pint.quantity._Quantity`): value of the attribute
+
+        Returns:
+            :obj:`str`: simple Python representation of a value of the attribute
+        """
+        if value:
+            return {
+                'magnitude': value.m,
+                'units': str(value.units),
+            }
+        else:
+            return None
+
+    def from_builtin(self, json):
+        """ Decode a simple Python representation (dict, list, str, float, bool, None) of a value of the attribute
+        that is compatible with JSON and YAML
+
+        Args:
+            json (:obj:`str`): simple Python representation of a value of the attribute
+
+        Returns:
+            :obj:`pint.quantity._Quantity`: decoded value of the attribute
+        """
+        if json:
+            return self.registry.Quantity(json['magnitude'], self.registry.parse_units(json['units']))
+        else:
+            return None
+
+    def get_excel_validation(self, sheet_models=None, doc_metadata_model=None):
+        """ Get Excel validation
+
+        Args:
+            sheet_models (:obj:`list` of :obj:`Model`, optional): models encoded as separate sheets
+            doc_metadata_model (:obj:`type`): model whose worksheet contains the document metadata
+
+        Returns:
+            :obj:`wc_utils.workbook.io.FieldValidation`: validation
+        """
+        validation = super(QuantityAttribute, self).get_excel_validation(sheet_models=sheet_models,
+                                                                         doc_metadata_model=doc_metadata_model)
+
+        validation.ignore_blank = self.none
+        if self.none:
+            input_message = ['Enter a quantity or blank.']
+            error_message = ['Value must be a quantity or blank.']
+        else:
+            input_message = ['Enter a quantity.']
+            error_message = ['Value must be a quantity.']
+
+        if self.unique:
+            input_message.append('Value must be unique.')
+            error_message.append('Value must be unique.')
+
+        default = self.get_default_cleaned_value()
+        if default:
+            input_message.append('Default: "{}".'.format(str(default)))
+
+        if validation.input_message:
+            validation.input_message += '\n\n'
+        validation.input_message += '\n\n'.join(input_message)
+
+        if validation.error_message:
+            validation.error_message += '\n\n'
+        validation.error_message += '\n\n'.join(error_message)
+
+        return validation
+
+
 def get_obj_units(obj):
     """ Get units used in a model object and related objects
 
@@ -284,7 +538,7 @@ def get_obj_units(obj):
     units = []
     for o in itertools.chain([obj], obj.get_related()):
         for attr in o.Meta.attributes.values():
-            if isinstance(attr, UnitAttribute):
+            if isinstance(attr, (UnitAttribute, QuantityAttribute)):
                 unit = getattr(o, attr.name)
                 if unit:
                     units.append(unit)
--- tests/sci/test_sci_units.py
+++ tests/sci/test_sci_units.py
@@ -61,6 +61,7 @@ class UnitAttributeTestCase(unittest.TestCase):
         self.assertFalse(attr.value_equal(registry1.parse_units('mol / l'), None))
         self.assertFalse(attr.value_equal(registry1.parse_units('g'), registry1.parse_units('l')))
         self.assertFalse(attr.value_equal(registry1.parse_units('ag'), registry1.parse_units('g')))
+        self.assertTrue(attr.value_equal(registry1.parse_units('ml'), registry1.parse_units('cm^3')))
 
     def test_clean(self):
         registry = pint.UnitRegistry()
@@ -147,3 +148,151 @@ class UnitAttributeTestCase(unittest.TestCase):
                           unit_attr_2=units_l)
 
         self.assertEqual(set(units.get_obj_units(model)), set([units_g, units_l]))
+
+    def test_get_excel_validation(self):
+        registry = pint.UnitRegistry()
+
+        attr = units.UnitAttribute(registry, none=True, unique=True,
+                                   default_cleaned_value=registry.parse_units('g'), choices=[registry.parse_units('g')])
+        attr.get_excel_validation()
+
+        attr = units.UnitAttribute(registry, none=False, unique=True,
+                                   default_cleaned_value=registry.parse_units('g'), choices=[registry.parse_units('g')])
+        attr.get_excel_validation()
+
+        attr = units.UnitAttribute(registry, none=True, unique=True)
+        attr.get_excel_validation()
+
+        attr = units.UnitAttribute(registry, none=False, unique=False)
+        attr.get_excel_validation()
+
+
+class QuantityAttributeTestCase(unittest.TestCase):
+    def test_init(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry,
+                                       choices=[registry.parse_units('s'), registry.parse_units('g')],
+                                       default=registry.parse_expression('4 s'),
+                                       default_cleaned_value=registry.parse_expression('3 g'))
+        self.assertEqual(len(attr.choices), 2)
+
+        attr = units.QuantityAttribute(registry)
+        self.assertEqual(attr.choices, None)
+
+        with self.assertRaisesRegex(ValueError, '`registry` must be an instance of'):
+            units.QuantityAttribute('registry')
+        with self.assertRaisesRegex(ValueError, '`default` must be an instance of'):
+            units.QuantityAttribute(registry, default='2 g')
+        with self.assertRaisesRegex(ValueError, '`default_cleaned_value` must be an instance of'):
+            units.QuantityAttribute(registry, default_cleaned_value='3 m')
+        with self.assertRaisesRegex(ValueError, 'choices must be instances of'):
+            units.QuantityAttribute(registry, choices=['g'])
+
+    def test_get_default(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry, default=registry.parse_expression('2 s'))
+        self.assertEqual(attr.get_default(), registry.Quantity(2, registry.parse_units('s')))
+
+    def test_get_default_cleaned_value(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry, default_cleaned_value=registry.parse_expression('3 g'))
+        self.assertEqual(attr.get_default_cleaned_value(), registry.parse_expression('3 g'))
+
+    def test_value_equal(self):
+        registry1 = unit_registry
+        registry2 = pint.UnitRegistry()
+        registry3 = pint.UnitRegistry()
+        attr = units.QuantityAttribute(unit_registry)
+
+        self.assertTrue(attr.value_equal(registry1.parse_expression('2 g'), registry1.parse_expression('2 g')))
+        self.assertFalse(attr.value_equal(registry1.parse_expression('2 g'), registry2.parse_expression('2 g')))
+        self.assertTrue(attr.value_equal(registry1.parse_expression('3 M'), registry1.parse_expression('3 mol / l')))
+        self.assertTrue(attr.value_equal(None, None))
+        self.assertFalse(attr.value_equal(None, registry1.parse_expression('2 mol / l')))
+        self.assertFalse(attr.value_equal('2 g', registry1.parse_expression('2 g')))
+        self.assertFalse(attr.value_equal(registry1.parse_expression('2  mol / l'), None))
+        self.assertFalse(attr.value_equal(registry1.parse_expression('2 g'), registry1.parse_expression('2 l')))
+        self.assertFalse(attr.value_equal(registry1.parse_expression('2 mg'), registry1.parse_expression('2 g')))
+        self.assertTrue(attr.value_equal(registry1.parse_expression('2000 mg'), registry1.parse_expression('2 g')))
+        self.assertTrue(attr.value_equal(registry1.parse_expression('2 ml'), registry1.parse_expression('2 cm^3')))
+
+    def test_clean(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry, default_cleaned_value=registry.parse_expression('g'))
+
+        self.assertEqual(attr.clean(''), (registry.parse_expression('g'), None))
+        self.assertEqual(attr.clean(None), (registry.parse_expression('g'), None))
+        self.assertEqual(attr.clean('s'), (registry.parse_expression('s'), None))
+        self.assertEqual(attr.clean('dimensionless'), (registry.parse_expression('dimensionless'), None))
+        self.assertNotEqual(attr.clean('dimensionless')[0], None)
+        self.assertEqual(attr.clean(registry.parse_expression('s')), (registry.parse_expression('s'), None))
+        self.assertNotEqual(attr.clean(1.)[1], None)
+        self.assertNotEqual(attr.clean('not_a_unit')[1], None)
+
+        attr = units.QuantityAttribute(registry, default_cleaned_value=None)
+        self.assertEqual(attr.clean(''), (None, None))
+        self.assertEqual(attr.clean(None), (None, None))
+        self.assertEqual(attr.clean('s'), (registry.parse_expression('s'), None))
+
+    def test_validate(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry, choices=[registry.parse_units('s')])
+
+        self.assertNotEqual(attr.validate(None, ''), None)
+        self.assertEqual(attr.validate(None, None), None)
+        self.assertEqual(attr.validate(None, registry.parse_expression('s')), None)
+        self.assertNotEqual(attr.validate(None, registry.parse_expression('g')), None)
+        self.assertNotEqual(attr.validate(None, 's'), None)
+        self.assertNotEqual(attr.validate(None, 2.), None)
+
+        attr = units.QuantityAttribute(registry)
+        self.assertEqual(attr.validate(None, None), None)
+        self.assertEqual(attr.validate(None, registry.parse_expression('s')), None)
+        self.assertEqual(attr.validate(None, registry.parse_expression('g')), None)
+
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry, none=False)
+        self.assertNotEqual(attr.validate(None, None), None)
+
+    def test_copy_value(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry)
+        unit = registry.parse_expression('s')
+        self.assertTrue(attr.value_equal(attr.copy_value(unit, {}), unit))
+
+    def test_serialize(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry)
+
+        self.assertEqual(attr.serialize(''), '')
+        self.assertEqual(attr.serialize(None), '')
+        self.assertEqual(attr.serialize(registry.parse_expression('s')), '1 second')
+        self.assertEqual(attr.serialize(registry.parse_expression('dimensionless')), '1 dimensionless')
+
+    def test_to_builtin(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry)
+
+        self.assertEqual(attr.to_builtin(''), None)
+        self.assertEqual(attr.to_builtin(None), None)
+        self.assertEqual(attr.to_builtin(registry.parse_expression('s')), {
+            'magnitude': 1.0,
+            'units': 'second',
+        })
+
+    def test_from_builtin(self):
+        registry = pint.UnitRegistry()
+        attr = units.QuantityAttribute(registry)
+
+        self.assertEqual(attr.from_builtin(''), None)
+        self.assertEqual(attr.from_builtin(None), None)
+        self.assertEqual(attr.from_builtin({'magnitude': 1, 'units': 's'}), registry.parse_expression('s'))
+
+    def test_get_excel_validation(self):
+        registry = pint.UnitRegistry()
+
+        attr = units.QuantityAttribute(registry, none=True, unique=True, default_cleaned_value=registry.parse_expression('1 g'))
+        attr.get_excel_validation()
+
+        attr = units.QuantityAttribute(registry, none=False, unique=False)
+        attr.get_excel_validation()
