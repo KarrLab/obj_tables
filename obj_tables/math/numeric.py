fe758017ee0e7a6e16463eda83733aa746c8a998
Jonathan Karr <jonrkarr@gmail.com>
2020-03-06 22:42:32 -0500

    adding pandas.DataFrame attribute

--- obj_tables/math/numeric.py
+++ obj_tables/math/numeric.py
@@ -13,7 +13,7 @@ import pandas
 
 __all__ = [
     'ArrayAttribute',
-    # 'TableAttribute',
+    'TableAttribute',
 ]
 
 
@@ -47,8 +47,8 @@ class ArrayAttribute(core.LiteralAttribute):
             raise ValueError('`max_length` must be an integer greater than or equal to `min_length`')
 
         super(ArrayAttribute, self).__init__(default=default, none_value=none_value,
-                                                  verbose_name=verbose_name, description=description,
-                                                  primary=primary, unique=unique)
+                                             verbose_name=verbose_name, description=description,
+                                             primary=primary, unique=unique)
 
         self.min_length = min_length
         self.max_length = max_length
@@ -188,3 +188,178 @@ class ArrayAttribute(core.LiteralAttribute):
             else:
                 dtype = None
             return numpy.array(json, dtype)
+
+
+class TableAttribute(core.LiteralAttribute):
+    """ pandas.DataFrame attribute
+
+    Attributes:
+        default (:obj:`pandas.DataFrame`): default value
+    """
+
+    def __init__(self, default=None, none_value=None, verbose_name='', description='',
+                 primary=False, unique=False):
+        """
+        Args:
+            default (:obj:`pandas.DataFrame`, optional): default value
+            none_value (:obj:`object`, optional): none value
+            verbose_name (:obj:`str`, optional): verbose name
+            description (:obj:`str`, optional): description
+            primary (:obj:`bool`, optional): indicate if attribute is primary attribute
+            unique (:obj:`bool`, optional): indicate if attribute value must be unique
+        """
+        if default is not None and not isinstance(default, pandas.DataFrame):
+            raise ValueError('`default` must be a `pandas.DataFrame` or `None`')
+
+        super(TableAttribute, self).__init__(default=default, none_value=none_value,
+                                             verbose_name=verbose_name, description=description,
+                                             primary=primary, unique=unique)
+
+    def deserialize(self, value):
+        """ Deserialize value
+
+        Args:
+            value (:obj:`str`): semantically equivalent representation
+
+        Returns:
+            :obj:`tuple` of `pandas.DataFrame`, `core.InvalidAttribute` or `None`: tuple of cleaned value and cleaning error
+        """
+        if self.default is not None:
+            dtype = self.default.values.dtype.type
+        else:
+            dtype = None
+
+        if value is None:
+            value = None
+            error = None
+        elif isinstance(value, str) and value == '':
+            value = None
+            error = None
+        elif isinstance(value, str):
+            try:
+                dict_value = json.loads(value)
+                index = dict_value.pop('_index')
+                value = pandas.DataFrame.from_dict(dict_value, dtype=dtype)
+                value.index = pandas.Index(index)
+                error = None
+            except:
+                value = None
+                error = 'Unable to parse pandas.DataFrame from string'
+        elif isinstance(value, dict):
+            try:
+                index = value.pop('_index')
+                value = pandas.DataFrame(value)
+                value = value.astype(dtype)
+                value.index = pandas.Index(index)
+                error = None
+            except:
+                value = None
+                error = 'Unable to parse pandas.DataFrame from dict'
+        elif isinstance(value, pandas.DataFrame):
+            error = None
+        else:
+            value = None
+            error = core.InvalidAttribute(self, [
+                ('TableAttribute must be None, an empty string, '
+                 'a JSON-formatted dict, a dict, '
+                 'or a pandas.DataFrame')
+            ])
+        return (value, error)
+
+    def validate(self, obj, value):
+        """ Determine if `value` is a valid value
+
+        Args:
+            obj (:obj:`Model`): class being validated
+            value (:obj:`pandas.DataFrame`): value of attribute to validate
+
+        Returns:
+            :obj:`core.InvalidAttribute` or None: None if attribute is valid, other return list of errors as an instance of `core.InvalidAttribute`
+        """
+        errors = []
+
+        if value is not None:
+            if not isinstance(value, pandas.DataFrame):
+                errors.append('Value must be an instance of `pandas.DataFrame`')
+            elif self.default is not None:
+                for elem in numpy.nditer(value.values):
+                    if not issubclass(elem.dtype.type, self.default.values.dtype.type):
+                        errors.append('Array elements must be of type `{}`'.format(self.default.values.dtype.type.__name__))
+                        break
+
+        if self.primary and (value is None or value.values.size == 0):
+            errors.append('{} value for primary attribute cannot be empty'.format(
+                self.__class__.__name__))
+
+        if errors:
+            return core.InvalidAttribute(self, errors)
+        return None
+
+    def validate_unique(self, objects, values):
+        """ Determine if the attribute values are unique
+
+        Args:
+            objects (:obj:`list` of :obj:`Model`): list of `Model` objects
+            values (:obj:`list` of :obj:`pandas.DataFrame`): list of values
+
+        Returns:
+           :obj:`core.InvalidAttribute` or None: None if values are unique, otherwise return a list of errors as an instance of `core.InvalidAttribute`
+        """
+        str_values = []
+        for v in values:
+            str_values.append(self.serialize(v))
+        return super(TableAttribute, self).validate_unique(objects, str_values)
+
+    def serialize(self, value):
+        """ Serialize string
+
+        Args:
+            value (:obj:`pandas.DataFrame`): Python representation
+
+        Returns:
+            :obj:`str`: simple Python representation
+        """
+        if value is not None:
+            dict_value = value.to_dict()
+            dict_value['_index'] = value.index.values.tolist()
+            return json.dumps(dict_value)
+        return ''
+
+    def to_builtin(self, value):
+        """ Encode a value of the attribute using a simple Python representation (dict, list, str, float, bool, None)
+        that is compatible with JSON and YAML
+
+        Args:
+            value (:obj:`pandas.DataFrame`): value of the attribute
+
+        Returns:
+            :obj:`dict`: simple Python representation of a value of the attribute
+        """
+        if value is None:
+            return None
+        else:
+            dict_value = value.to_dict()
+            dict_value['_index'] = value.index.values.tolist()
+            return dict_value
+
+    def from_builtin(self, json):
+        """ Decode a simple Python representation (dict, list, str, float, bool, None) of a value of the attribute
+        that is compatible with JSON and YAML
+
+        Args:
+            json (:obj:`dict`): simple Python representation of a value of the attribute
+
+        Returns:
+            :obj:`pandas.DataFrame`: decoded value of the attribute
+        """
+        if json is None:
+            return None
+        else:
+            if self.default is not None:
+                dtype = self.default.values.dtype.type
+            else:
+                dtype = None
+            index = json.pop('_index')
+            value = pandas.DataFrame.from_dict(json, dtype=dtype)
+            value.index = pandas.Index(index)
+            return value
--- tests/math/test_math_numeric.py
+++ tests/math/test_math_numeric.py
@@ -10,11 +10,12 @@ from obj_tables import core
 import mock
 import numpy
 import obj_tables.math.numeric
+import pandas
 import unittest
 
 
-class MathAttributeTestCase(unittest.TestCase):
-    def test_ArrayAttribute(self):
+class ArrayAttributeTestCase(unittest.TestCase):
+    def test(self):
         # constructor
         attr = obj_tables.math.numeric.ArrayAttribute()
         self.assertEqual(attr.get_default(), None)
@@ -93,3 +94,130 @@ class MathAttributeTestCase(unittest.TestCase):
 
         attr = obj_tables.math.numeric.ArrayAttribute(primary=True, default=numpy.array([1.1, 2.2]))
         numpy.testing.assert_equal(attr.from_builtin([1, 2]), numpy.array([1, 2]))
+
+
+class TableAttributeTestCase(unittest.TestCase):
+    def test__init__(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+        self.assertEqual(attr.get_default(), None)
+
+        default_values = {'a': [1, 2]}
+        default = pandas.DataFrame(default_values)
+        attr = obj_tables.math.numeric.TableAttribute(default=default)
+        self.assertTrue(attr.get_default().equals(default))
+
+        with self.assertRaisesRegex(ValueError, '`default` must be a `pandas.DataFrame` or `None`'):
+            obj_tables.math.numeric.TableAttribute(default=default_values)
+
+    def test_validate(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+        self.assertEqual(attr.validate(None, None), None)
+        self.assertNotEqual(attr.validate(None, []), None)
+
+        attr = obj_tables.math.numeric.TableAttribute(default=pandas.DataFrame({'a': [1, 2]}))
+        print(attr.validate(None, pandas.DataFrame({'a': [1, 2]}).astype(numpy.int64)))
+        self.assertEqual(attr.validate(None, pandas.DataFrame({'a': [1, 2]}).astype(numpy.int64)), None)
+        self.assertNotEqual(attr.validate(None, pandas.DataFrame({'a': [1., 2.]})), None)
+
+        attr = obj_tables.math.numeric.TableAttribute(primary=True)
+        self.assertNotEqual(attr.validate(None, None), None)
+
+    def test_validate_unique(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+
+        self.assertEqual(attr.validate_unique([], [
+            pandas.DataFrame({'a': [1, 2]}),
+            pandas.DataFrame({'a': [3, 4]}),
+        ]), None)
+
+        self.assertEqual(attr.validate_unique([], [
+            pandas.DataFrame({'a': [1, 2]}),
+            None,
+        ]), None)
+
+        self.assertNotEqual(attr.validate_unique([], [
+            pandas.DataFrame({'a': [1, 2]}),
+            pandas.DataFrame({'a': [1, 2]}),
+        ]), None)
+
+        self.assertNotEqual(attr.validate_unique([], [None, None]), None)
+
+    def test_serialize_deserialize(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+
+        value = pandas.DataFrame({'a': [1, 2], 'b': [3, 4]})
+        deserialized_value, error = attr.deserialize(attr.serialize(value))
+        self.assertEqual(error, None)
+        print(value.index)
+        print(deserialized_value.index)
+        self.assertTrue(deserialized_value.equals(value))
+        self.assertEqual(deserialized_value.values.dtype.type, numpy.int64)
+
+        value = pandas.DataFrame({'a': [1., 2.], 'b': [3., 4.]})
+        deserialized_value, error = attr.deserialize(attr.serialize(value))
+        self.assertEqual(error, None)
+        self.assertTrue(deserialized_value.equals(value))
+        self.assertEqual(deserialized_value.values.dtype.type, numpy.float64)
+
+    def test_deserialize(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+        self.assertEqual(attr.deserialize(None), (None, None))
+        self.assertEqual(attr.deserialize(''), (None, None))
+
+        value, error = attr.deserialize('{"a": {"0": 1, "1": 2}, "_index": [0, 1]}')
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1, 2]})))
+        self.assertEqual(error, None)
+
+        value, error = attr.deserialize('{"a": {"0": 1.0, "1": 2.0}, "_index": [0, 1]}')
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(error, None)
+
+        value, error = attr.deserialize({'a': [1., 2.], '_index': [0, 1]})
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(error, None)
+
+        value, error = attr.deserialize(pandas.DataFrame({'a': [1., 2.]}))
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(error, None)
+
+        attr = obj_tables.math.numeric.TableAttribute(default=pandas.DataFrame({'a': [1., 2.]}))
+
+        value, error = attr.deserialize('{"a": {"0": 1, "1": 2}, "_index": [0, 1]}')
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(value.values.dtype.type, numpy.float64)
+        self.assertEqual(error, None)
+
+        value, error = attr.deserialize({'a': [1, 2], '_index': [0, 1]})
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(value.values.dtype.type, numpy.float64)
+        self.assertEqual(error, None)
+
+        value, error = attr.deserialize(pandas.DataFrame({'a': [1., 2.]}))
+        self.assertTrue(value.equals(pandas.DataFrame({'a': [1., 2.]})))
+        self.assertEqual(value.values.dtype.type, numpy.float64)
+        self.assertEqual(error, None)
+
+        self.assertNotEqual(attr.deserialize('x')[1], None)
+        self.assertNotEqual(attr.deserialize(1.)[1], None)
+
+        _, error = attr.deserialize({'a': [1., 2.], 'b': [3.], '_index': [0, 1]})
+        self.assertNotEqual(error, None)
+
+    def test_to_from_builtin(self):
+        attr = obj_tables.math.numeric.TableAttribute()
+        self.assertEqual(attr.to_builtin(None), None)
+
+        raw_value = {'a': [1, 2], 'b': [3, 4]}
+        value = pandas.DataFrame(raw_value)
+
+        dict_value = {'a': {0: 1, 1: 2}, 'b': {0: 3, 1: 4}, '_index': [0, 1]}
+        self.assertEqual(attr.to_builtin(value), dict_value)
+
+        self.assertEqual(attr.from_builtin(None), None)
+        self.assertTrue(attr.from_builtin(dict_value).equals(value))
+        self.assertTrue(attr.from_builtin(attr.to_builtin(value)).equals(value))
+
+        raw_value = {'a': [1., 2.], 'b': [3., 4.]}
+        value = pandas.DataFrame(raw_value)
+        attr = obj_tables.math.numeric.TableAttribute(default=value)
+        self.assertTrue(attr.from_builtin(attr.to_builtin(value)).equals(value))
